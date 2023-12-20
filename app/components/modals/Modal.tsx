2425a70a6d4576719ca4b3aa9564c3d3f0fa0f92
Derrick Ankrah <darhin97@gmail.com>
2023-12-20 19:28:43 +0000

    fixes

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -71,6 +71,7 @@ const Modal: React.FC<ModalProps> = (props) => {
       <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 ">
         <div className="relative w-full md:w-4/6  lg:w-3/6 xl:w-2/55 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* content */}
+
           <div
             className={`translate duration-300 h-full ${
               showModal ? "translate-y-0" : "translate-y-full"
